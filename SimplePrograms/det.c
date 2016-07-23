6dc5656793d8003bc26e430312d271e233a842de
ShekharReddy4 <reddyshekhar619@gmail.com>
2016-07-23 18:08:41 +0530

    added the determinant of a 3x3 matrix

--- SimplePrograms/det.c
+++ SimplePrograms/det.c
@@ -3,40 +3,33 @@ int* minor(int i, int *mat, int ord){
 
 }
 
-
 int determinant(int *matrix, int order){
-
-  if (order!=2) {
+  int sum = 0;
+  if (order != 2) {
     for ( i = 0; i < order; i++) {
-      sum = sum + determinant(minor(i, matrix, order));
+      sum = sum + (matrix[0][i] * determinant(minor(i, 0, matrix, order)));
     }
     order--;
     return sum;
-
   }
   else{
-
-    for (i = 0; i < order; i++) {
-      
+    int mul = (matrix[0][0] * matrix[1][1]) - (matrix[1][0] * matrix[0][1]);
+    return mul;
     }
   }
-
 }
 
-
-
-
 void  main() {
-
   int order=3;
   int matrix[3][3];
+  int detofmatrix;
+
   printf("enter the elements in the matrix\n" );
   for ( i = 0; i < order; i++) {
     for ( j = 0; j < order; j++) {
       scanf("%d",matrix[i][j]);
     }
   }
-
-  printf("determinant of the given matrix is :  %d", determinant(matrix,order));
-
+  detofmatrix = determinant(matrix,order);
+  printf("determinant of the given matrix is :  %d", detofmatrix);
 }

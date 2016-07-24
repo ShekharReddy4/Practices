70c9b8266dd53d471524b95dbefc84a6e46d59a9
ShekharReddy4 <reddyshekhar619@gmail.com>
2016-07-24 21:26:30 +0530

    added the 2x2 matrix as struct

--- SimplePrograms/det.c
+++ SimplePrograms/det.c
@@ -4,6 +4,10 @@ struct array{
   int matrix[3][3];
 };
 
+struct array2{
+  int matrix[2][2];
+};
+
 struct array transpose_matrix(struct array matri, int order){
   struct array tmatrix;
   int i,j;
@@ -15,18 +19,35 @@ struct array transpose_matrix(struct array matri, int order){
   return tmatrix;
 }
 
-/*int* minor(int i, int *mat, int ord){
-  ord--;
+int minor(struct mat, int order, int row, int col){
+  struct array2 mat2;
+  int i,j,k;
+
+  for ( i = 0, k = 0; i < order; i++) {
+    for ( j = 0; j < order; j++) {
+      scanf("%d",&mat.matrix[i][j]);
+    }
+  }
 
 }
-*/
 
-int determinant(int *matrix, int order){
+struct array adjoint(struct array omat, order){
+  struct array radjmat;
+  int i,j,k;
+  for ( i = 0; i < order; i++) {
+    for ( j = 0; j < order; j++) {
+        radjmat.matrix[i][j] = ;
+    }
+  }
+}
+
+
+int determinant(struct array matric, int order){
   int sum = 0;
   int i;
   if (order != 2) {
     for ( i = 0; i < order; i++) {
-      sum = sum + (matrix[0][i] * determinant(minor(i, 0, matrix, order)));
+      sum = sum + (matric.matrix[0][i] * determinant(minor(i, 0, matric, order)));
     }
     order--;
     return sum;
@@ -40,8 +61,8 @@ int determinant(int *matrix, int order){
 
 
 void  main() {
-  int order=3;
-  struct array tpointer,mat;
+  int order=3, detofmatrix;
+  struct array tpointer,mat,adjmat,invmatrix;
   int i,j;
 
   printf("enter the elements in the matrix\n" );
@@ -50,18 +71,47 @@ void  main() {
       scanf("%d",&mat.matrix[i][j]);
     }
   }
-  //detofmatrix = determinant(matrix,order);
-  //printf("determinant of the given matrix is :  %d", detofmatrix);
 
+  //transpose of the given matrix
   printf("transpose of the given matrix is : \n");
   tpointer = transpose_matrix(mat, 3);
+
   for ( i = 0; i < order; i++) {
     for ( j = 0; j < order; j++) {
-
       printf("%d    ", tpointer.matrix[i][j]);
     }
     printf("\n");
+  }
 
+
+  //determinant of a given matrix
+  printf("determinant of the given matrix is :  %d", detofmatrix);
+  detofmatrix = determinant(mat,order);
+
+  //adjoint of the given matrix
+  adjmat = adjoint(mat, order);
+  for ( i = 0; i < order; i++) {
+    for ( j = 0; j < order; j++) {
+      printf("%d    ", adjmat.matrix[i][j]);
+    }
+    printf("\n");
+  }
+
+  //inverse of the given matrix
+  if (detofmatrix!=0) {
+    for ( i = 0; i < order; i++) {
+      for ( j = 0; j < order; j++) {
+          invmatrix.matrix[i][j] = (adjmat.matrix[i][j]/detofmatrix);
+      }
+      printf("\n");
+    }
+  }
+
+  for ( i = 0; i < order; i++) {
+    for ( j = 0; j < order; j++) {
+      printf("%d    ", invmatrix.matrix[i][j]);
+    }
+    printf("\n");
   }
 
 }

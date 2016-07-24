cb8a8b4464653e0367c1f9f136b89880b355e2b6
ShekharReddy4 <reddyshekhar619@gmail.com>
2016-07-24 20:20:08 +0530

    added struct for the functions to functoion

--- SimplePrograms/det.c
+++ SimplePrograms/det.c
@@ -1,18 +1,25 @@
-int* minor(int i, int *mat, int ord){
-  ord--;
+#include <stdio.h>
 
-}
+struct array{
+  int matrix[3][3];
+};
 
-int* transpose_matrix(int *matrix, int order){
-  int *tmatrix;
-  int i,j,k;
+struct array transpose_matrix(struct array matri, int order){
+  struct array tmatrix;
+  int i,j;
   for ( i = 0; i < order; i++) {
-    for ( j = 0; j < count; j++) {
-        matrix[i][j] = tmatrix[j][i];
+    for ( j = 0; j < order; j++) {
+        tmatrix.matrix[j][i] = matri.matrix[i][j];
     }
   }
   return tmatrix;
 }
+
+/*int* minor(int i, int *mat, int ord){
+  ord--;
+
+}
+*/
 
 int determinant(int *matrix, int order){
   int sum = 0;
@@ -31,18 +38,30 @@ int determinant(int *matrix, int order){
   }
 }
 
+
 void  main() {
   int order=3;
-  int matrix[3][3];
-  int detofmatrix;
+  struct array tpointer,mat;
   int i,j;
 
   printf("enter the elements in the matrix\n" );
   for ( i = 0; i < order; i++) {
     for ( j = 0; j < order; j++) {
-      scanf("%d",matrix[i][j]);
+      scanf("%d",&mat.matrix[i][j]);
     }
   }
-  detofmatrix = determinant(matrix,order);
-  printf("determinant of the given matrix is :  %d", detofmatrix);
+  //detofmatrix = determinant(matrix,order);
+  //printf("determinant of the given matrix is :  %d", detofmatrix);
+
+  printf("transpose of the given matrix is : \n");
+  tpointer = transpose_matrix(mat, 3);
+  for ( i = 0; i < order; i++) {
+    for ( j = 0; j < order; j++) {
+
+      printf("%d    ", tpointer.matrix[i][j]);
+    }
+    printf("\n");
+
+  }
+
 }
